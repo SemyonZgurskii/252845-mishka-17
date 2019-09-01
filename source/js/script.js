d46226b0b644471fe7084b66cd4cce63a17840fb
Семен Згурский <saymon221090@gmail.com>
2019-09-01 08:38:18 +0300

    перед установкой picturefill

--- source/js/script.js
+++ source/js/script.js
@@ -4,11 +4,12 @@ var sideMenu = document.querySelector(".header__side-menu");
 var weekProductOrder = document.querySelector(".week-product__order-button");
 var modal = document.querySelector(".modal-bg");
 var submitModal = document.querySelector(".modal__button");
+var cartAdd = document.querySelector(".product__basket");
 
 mainNav.classList.remove(".header-navigation--nojs");
 sideMenu.classList.remove(".header__side-menu--nojs");
 
-menuToggle.addEventListener('click', function() {
+menuToggle.addEventListener("click", function() {
   if (menuToggle.classList.contains("header__menu-toggle--closed")) {
     menuToggle.classList.remove("header__menu-toggle--closed");
     menuToggle.classList.add("header__menu-toggle--opened");
@@ -18,10 +19,27 @@ menuToggle.addEventListener('click', function() {
   }
 });
 
-weekProductOrder.addEventListener('click', function() {
+weekProductOrder.addEventListener("click", function(evt) {
+  evt.preventDefault();
   modal.classList.add("modal-bg--show");
 });
 
-modal.addEventListener('click', function() {
-  modal.classList.remove("modal-bg--show");
+// cartAdd.addEventListener("click", function(evt) {
+//   evt.preventDefault();
+//   modal.classList.add("modal-bg--show");
+// });
+
+modal.addEventListener("click", function(evt) {
+  if (evt.target.closest(".modal-bg") === null) {
+    modal.classList.remove("modal-bg--show");
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (modal.classList.contains("modal-bg--show")) {
+      evt.preventDefault();
+      modal.classList.remove("modal-bg--show");
+    }
+  }
 });
